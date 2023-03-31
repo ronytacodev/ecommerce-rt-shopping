872ddbfb19c8b6bc1acc9d5ebed2cac82e301d93
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-03-31 08:20:57 -0500

    apply filtering isotope to collection list

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// filtering collection with Isotope library js : init Isotope
+
+var $grid = $('.collection-list').isotope({
+    // options
+  });
+  // filter items on button click
+  $('.filter-button-group').on( 'click', 'button', function() {
+    var filterValue = $(this).attr('data-filter');
+    $grid.isotope({ filter: filterValue });
+  });
+
+// custom js script to filtering collection
