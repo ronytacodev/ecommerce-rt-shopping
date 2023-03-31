a18734fc0614c68e3936a68a8338dc4632b58328
RonyTCdev <64666995+ronyfringe@users.noreply.github.com>
2023-03-31 08:30:48 -0500

    add script  remove, add class button filter

--- js/script.js
+++ js/script.js
@@ -1,12 +1,19 @@
 // filtering collection with Isotope library js : init Isotope
-
 var $grid = $('.collection-list').isotope({
     // options
   });
   // filter items on button click
   $('.filter-button-group').on( 'click', 'button', function() {
     var filterValue = $(this).attr('data-filter');
+    resetFilterBtns();
+    $(this).addClass('active-filter-btn');
     $grid.isotope({ filter: filterValue });
   });
 
-// custom js script to filtering collection
+// custom js script to add filtering collection: this is to paint button selected filter
+var filterBtns = $('.filter-button-group').find('button');
+function resetFilterBtns() {
+    filterBtns.each(function(){
+        $(this).removeClass('active-filter-btn');
+    });
+}
